3310dc050bd6e163974bcf170a7563c8bb4701b9
Gabriela Nery <gneri94@gmail.com>
2024-07-01 11:39:23 -0300

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,17 +2,6 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = 3005;
-//index.js
-(async () => {
-  const database = require("../config/database");
-  database.sequelize.sync()
-    .then((e: any) => {
-      console.log(e);
-    })
-    .catch((e: any) => {
-      console.log(e);
-    });
-})();
 
 const server = http.createServer((req: any, res: any) => {
   res.statusCode = 200;
