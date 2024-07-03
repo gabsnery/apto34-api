dce7e3cd4e7c2533f0bb78ccac849708b5cc1a2b
Gabriela Nery <gneri94@gmail.com>
2024-07-03 16:22:18 -0300

    fix: return photo url insted of id

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -218,7 +218,7 @@ async function transformProducts(products: any[]): Promise<ProductResponse[]> {
         const transformedProduct: ProductResponse = {
             id: product.id,
             produtoSubcategoria: transformedSubcategories,
-            photos: product.photo?.filter((ite: any) => ite.thumbnail === false).map((item: any) => encryptId(item.id.toString())),
+            photos: product.photo?.filter((ite: any) => ite.thumbnail === false).map((item: any) => encryptId(item.url.toString())),
             thumbnails: product.photo?.filter((ite: any) => ite.thumbnail === true).map((item: any) => item.url),
             cores: product.color?.map((item: any) => {
                 return ({
